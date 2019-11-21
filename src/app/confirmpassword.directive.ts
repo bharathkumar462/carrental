c0668ef55abe84071478e255d93fcd5b8c4452b0
budayakumar <budayakumar@nextsphere.com>
2019-11-21 15:46:14 +0530

    update21nov4pm

--- src/app/confirmpassword.directive.ts
+++ src/app/confirmpassword.directive.ts
@@ -41,38 +41,3 @@ export class ConfirmPassword implements Validator {
     }
 
 }
-
-
-
-
-
-
-    //     constructor(@Attribute('compare') public comparer: string,
-    //     @Attribute('parent') public parent: string){}
-
-    // validate(c:FormControl): {[key: string]: any} {
-    // let e = c.root.get(this.comparer);
-
-    // // value not equal in verify control
-    // if (e && c.value !== e.value && !this.isParent) {
-    // return {"compare": true};
-    // }
-
-    // // user typing in password and match
-    // if (e && c.value === e.value && this.isParent) {
-    // delete e.errors['compare'];
-    // if (!Object.keys(e.errors).length) e.setErrors(null);
-    // }
-
-    // // user typing in password and mismatch
-    // if (e && c.value !== e.value && this.isParent) {
-    // e.setErrors({ "compare": true });
-    // }
-    // }
-
-    // private get isParent() {
-    // if (!this.parent) 
-    // return false;
-
-    // return this.parent === 'true' ? true: false;
-    // }
